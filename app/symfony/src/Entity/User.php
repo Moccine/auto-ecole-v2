c7c544a0682519ef6dcad4c4329b1a121d4341ec
msow <msow@kernix.com>
2020-11-28 12:41:29 +0100

    delete asset

---
+++ app/symfony/src/Entity/User.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Traits\IdentifiableTrait;
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ *  @UniqueEntity(fields={"email"})
+ */
+class User
+{
+
+    use IdentifiableTrait;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $mobile;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $jobTitle;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $username;
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getMobile(): ?string
+    {
+        return $this->mobile;
+    }
+
+    public function setMobile(string $mobile): self
+    {
+        $this->mobile = $mobile;
+
+        return $this;
+    }
+
+    public function getJobTitle(): ?string
+    {
+        return $this->jobTitle;
+    }
+
+    public function setJobTitle(string $jobTitle): self
+    {
+        $this->jobTitle = $jobTitle;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $userName): self
+    {
+        $this->username = $userName;
+
+        return $this;
+    }
+}
